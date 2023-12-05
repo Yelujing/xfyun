35cb6161cbb998382ec71774ef789e356d9b59c1
Yelujing <108774627+Yelujing@users.noreply.github.com>
2023-12-05 15:59:55 +0800

    Add files via upload

---
+++ SparkApi.py
@@ -0,0 +1,143 @@
+import _thread as thread
+import base64
+import datetime
+import hashlib
+import hmac
+import json
+from urllib.parse import urlparse
+import ssl
+from datetime import datetime
+from time import mktime
+from urllib.parse import urlencode
+from wsgiref.handlers import format_date_time
+
+import websocket  # 使用websocket_client
+
+answer = ""
+content = ""
+
+
+# 构造方法，用于获得密钥
+class Ws_Param(object):
+    # 初始化
+    def __init__(self, APPID, APIKey, APISecret, Spark_url):
+        self.APPID = APPID
+        self.APIKey = APIKey
+        self.APISecret = APISecret
+        self.host = urlparse(Spark_url).netloc  # 通过 urlparse(Spark_url).netloc 获取 Spark_url 的主机部分，并将其赋值给类的 host 属性。
+        self.path = urlparse(Spark_url).path  # 通过 urlparse(Spark_url).path 获取 Spark_url 的路径部分，并将其赋值给类的 path 属性。
+        self.Spark_url = Spark_url
+
+    # 生成url，需要了解星火的鉴权规则
+    def create_url(self):
+        # 生成RFC1123格式的时间戳
+        now = datetime.now()
+        date = format_date_time(mktime(now.timetuple()))  # 规定的日期格式规则
+
+        # 拼接字符串
+        signature_origin = "host: " + self.host + "\n"
+        signature_origin += "date: " + date + "\n"
+        signature_origin += "GET " + self.path + " HTTP/1.1"
+        # 上方拼接生成的signature_origin字符串如下
+        # host: spark-api.xf-yun.com
+        # date: Fri, 05 May 2023 10:43:39 GMT
+        # GET /v1.1/chat HTTP/1.1
+
+        # 进行hmac-sha256进行加密
+        signature_sha = hmac.new(self.APISecret.encode('utf-8'), signature_origin.encode('utf-8'),
+                                 digestmod=hashlib.sha256).digest()  # 将APISecret、signature_origin加密赋值给signature_sha
+
+        signature_sha_base64 = base64.b64encode(signature_sha).decode(encoding='utf-8')  # 进行 Base64 编码，并将其转换为字符串，以便后续使用
+
+        authorization_origin = f'api_key="{self.APIKey}", algorithm="hmac-sha256", headers="host date request-line", signature="{signature_sha_base64}"'  # 可读，不可读，可读
+
+        authorization = base64.b64encode(authorization_origin.encode('utf-8')).decode(encoding='utf-8')
+
+        # 将请求的鉴权参数组合为字典
+        v = {
+            "authorization": authorization,
+            "date": date,
+            "host": self.host
+        }
+        # 拼接鉴权参数，生成url
+        url = self.Spark_url + '?' + urlencode(v)  # 将字典中的键值对转换为 URL 查询字符串的格式
+        # 此处打印出建立连接时候的url,参考本demo的时候可取消上方打印的注释，比对相同参数时生成的url与自己代码生成的url是否一致
+        return url
+
+
+# 收到websocket错误的处理
+def on_error(ws, error):
+    print("### error:", error)
+
+
+# 收到websocket关闭的处理
+def on_close(ws, one, two):
+    print(" ")
+
+
+# 收到websocket连接建立的处理
+def on_open(ws):
+    thread.start_new_thread(run, (ws,))
+
+
+def run(ws, *args):
+    data = json.dumps(gen_params(appid=ws.appid, domain=ws.domain, question=ws.question))
+    ws.send(data)
+
+
+# 收到websocket消息的处理
+def on_message(ws, message):
+    global content
+    # print(message)
+    data = json.loads(message)
+    code = data['header']['code']
+    if code != 0:
+        print(f'请求错误: {code}, {data}')
+        ws.close()
+    else:
+        choices = data["payload"]["choices"]
+        status = choices["status"]
+        content = choices["text"][0]["content"]  # 从choices中获取第一个text内容的content字段值，即回答的内容
+        print(content, end="")  # 将回答输出到控制台
+        global answer
+        answer += content  # 将回答保存到answer中
+        # print(1)
+        if status == 2:  # 状态为2表示回答结束，断开连接
+            ws.close()
+
+
+def gen_params(appid, domain, question):
+    """
+    通过appid和用户的提问来生成请参数
+    """
+    data = {
+        "header": {
+            "app_id": appid,
+            "uid": "1234"
+        },
+        "parameter": {
+            "chat": {
+                "domain": domain,
+                "temperature": 0.6,  # 核采样阈值，默认0.5
+                "max_tokens": 4096  # 模型回答的tokens的最大长度，默认2048，v1.5最大为14096，其余为18192
+            }
+        },
+        "payload": {
+            "message": {
+                "text": question
+            }
+        }
+    }
+    return data
+
+
+def main(appid, api_key, api_secret, Spark_url, domain, question):
+    # print("星火:")
+    wsParam = Ws_Param(appid, api_key, api_secret, Spark_url)
+    websocket.enableTrace(False)
+    wsUrl = wsParam.create_url()
+    ws = websocket.WebSocketApp(wsUrl, on_message=on_message, on_error=on_error, on_close=on_close, on_open=on_open)
+    ws.appid = appid
+    ws.question = question
+    ws.domain = domain
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
---
+++ test.py
@@ -0,0 +1,125 @@
+# test.py 文件
+import base64
+import os
+from idlelib import window
+
+import SparkApi
+import tkinter as tk
+
+# 以下密钥信息从控制台获取
+appid = "2b7ab044"  # 填写控制台中获取的 APPID 信息
+api_secret = "YTFiNmEzZTZmZTk3MGJhYzNkODIwZDc3"  # 填写控制台中获取的 APISecret 信息
+api_key = "fd769aa40b92619985003c8000298d22"  # 填写控制台中获取的 APIKey 信息
+
+# 用于配置大模型版本，默认“general/generalv2”
+domain = "general"  # v1.5版本
+# domain = "generalv2"  # v2.0版本
+# 云端环境的服务地址
+Spark_url = "ws://spark-api.xf-yun.com/v1.1/chat"  # v1.5环境的地址
+# Spark_url = "ws://spark-api.xf-yun.com/v2.1/chat"  # v2.0环境的地址
+
+text = []
+
+
+# 获取消息长度
+def getlength(text):
+    length = 0
+    for content in text:
+        temp = content["content"]
+        leng = len(temp)
+        length += leng
+    return length
+
+
+# 检查消息长度，控制在8000以内
+def checklen(text):
+    while getlength(text) > 8000:
+        del text[0]
+    return text
+
+
+# 获取消息内容
+def getText(role, content):
+    jsoncon = {}
+    jsoncon["role"] = role
+    jsoncon["content"] = content
+    text.append(jsoncon)
+    return text
+
+
+if __name__ == '__main__':
+
+    # 全局变量
+    # answer = ""
+    # question = []
+
+
+    # 发送问题的函数
+    def send_question():
+        # global answer, question
+        # user_input = entry.get()  # 获取用户输入的问题
+        # question.append({"role": "user", "content": user_input})
+        # SparkApi.answer = ""  # 清空答案
+        # SparkApi.main(appid, api_key, api_secret, Spark_url, domain, question)  # 发送问题
+        # question.pop()  # 移除已发送的问题
+        # entry.delete(0, tk.END)  # 清空用户输入
+        # update_answer()
+
+        question = checklen(getText("user", entry.get()))   # 获取用户输入的问题
+        SparkApi.answer = ""  # 清空答案
+        entry.delete(0, tk.END)  # 清空用户输入
+        SparkApi.main(appid, api_key, api_secret, Spark_url, domain, question)  # 发送问题
+        getText("assistant", SparkApi.answer)  # 存储回答
+        text.config(state=tk.NORMAL)
+        text.insert(tk.END, f"\n星火: {SparkApi.content}\n")  # 在文本框中显示答案
+        text.config(state=tk.DISABLED)
+        text.see(tk.END)  # 将滚动条移动到最后显示最新的答案
+
+    # 更新答案文本框
+    # def update_answer():
+    #     global answer
+    #     # text.delete("1.0", tk.END)  # 清空文本框内容
+    #     if SparkApi.answer != answer:  # 检查是否有新的答案
+    #         answer = SparkApi.answer
+    #         text.config(state=tk.NORMAL)
+    #         text.insert(tk.END, f"\n星火: {answer}\n")  # 在文本框中显示答案
+    #         text.config(state=tk.DISABLED)
+    #         text.see(tk.END)  # 将滚动条移动到最后显示最新的答案
+
+
+    # 主窗口
+
+    root = tk.Tk()
+    root.title("7#401自己的miniChatGPT")
+    # 设置窗体图标
+    from icon import img
+
+    # 读取base64转码后的数据，并设置压缩图标
+    picture = open("picture.ico", "wb+")
+    picture.write(base64.b64decode(img))
+    picture.close()
+    root.iconbitmap('picture.ico')
+    os.remove("picture.ico")
+
+    # 创建文本框用于显示回答
+    text = tk.Text(root, height=20, width=50, state=tk.DISABLED)
+    text.pack()
+
+    # # 创建滚动条
+    # scrollbar = tk.Scrollbar(root)
+    # scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    # scrollbar.config(command=text.yview)
+
+    # 创建文本框用于用户输入问题
+    entry = tk.Entry(root, width=50)
+    entry.pack()
+
+    # 创建发送按钮
+    send_button = tk.Button(root, text="发送", command=send_question)
+    send_button.pack()
+
+    # 更新答案
+    # update_answer()
+
+    # 运行主循环
+    root.mainloop()
